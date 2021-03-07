7f0b21df0118662a1dcbdb5862a0519502f254c0
rbehal <rahulbehal01@hotmail.com>
2021-03-07 16:10:39 -0500

    Added tab functionality with separate image widgets

--- imageAnalysis/PyQt-Image-Viewer/ImageViewer.py
+++ imageAnalysis/PyQt-Image-Viewer/ImageViewer.py
@@ -5,62 +5,178 @@ from PIL.ImageQt import ImageQt
 from PIL import Image
 import cv2
 import numpy as np
+import os, re
 
 class ImageViewer:
     ''' Basic image viewer class to show an image with zoom and pan functionaities.
         Requirement: Qt's Qlabel widget name where the image will be drawn/displayed.
     '''
-    def __init__(self, qlabel):
-        self.qlabel_image = qlabel                            # widget/window name where image is displayed (I'm usiing qlabel)
-        self.qimage_scaled = QImage()                         # scaled image to fit to the size of qlabel_image
-        self.qpixmap = QPixmap()                              # qpixmap to fill the qlabel_image
+    def __init__(self, imageLabels, window):
+        self.imageLabels = {"BF": imageLabels[0], "TR": imageLabels[1]}
+        self.currImageLabel = imageLabels[0]  # Widget/window name where image is displayed
+        self.window = window
+        self.qimage_scaled = QImage()         # Scaled image to fit to the size of currImageLabel
+        self.qpixmap = QPixmap()              # QPixmap to fill the currImageLabel
 
-        self.zoomX = 1              # zoom factor w.r.t size of qlabel_image
-        self.position = [0, 0]      # position of top left corner of qimage_label w.r.t. qimage_scaled
-        self.panFlag = False        # to enable or disable pan
+        self.zoomX = 1                        # Zoom factor w.r.t size of currImageLabel
+        self.position = [0, 0]                # Position of top left corner of currImageLabel w.r.t. qimage_scaled
+        self.panFlag = False                  # to enable or disable pan
 
-        self.folders = {"BF": None, "TR": None}
+        self.folders = {"BASE": None, "BF": None, "TR": None}
+        self.imageList = {"BF": [], "TR": []}
+        self.currImageList = None
+        self.currImageIdx = -1 
+        self.numImages = -1
+        self.items = []
 
-        self.qlabel_image.setSizePolicy(QtWidgets.QSizePolicy.Ignored, QtWidgets.QSizePolicy.Ignored)
+        self.currImageLabel.setSizePolicy(QtWidgets.QSizePolicy.Ignored, QtWidgets.QSizePolicy.Ignored)
         self.__connectEvents()
 
     def __connectEvents(self):
         # Mouse events
-        self.qlabel_image.mousePressEvent = self.mousePressAction
-        self.qlabel_image.mouseMoveEvent = self.mouseMoveAction
-        self.qlabel_image.mouseReleaseEvent = self.mouseReleaseAction
+        self.currImageLabel.mousePressEvent = self.mousePressAction
+        self.currImageLabel.mouseMoveEvent = self.mouseMoveAction
+        self.currImageLabel.mouseReleaseEvent = self.mouseReleaseAction
 
     def onResize(self):
-        ''' things to do when qlabel_image is resized '''
-        self.qpixmap = QPixmap(self.qlabel_image.size())
+        ''' things to do when currImageLabel is resized '''
+        self.qpixmap = QPixmap(self.currImageLabel.size())
         self.qpixmap.fill(QtCore.Qt.gray)
-        self.qimage_scaled = self.qimage.scaled(self.qlabel_image.width() * self.zoomX, self.qlabel_image.height() * self.zoomX, QtCore.Qt.KeepAspectRatioByExpanding)
+        self.qimage_scaled = self.qimage.scaled(self.currImageLabel.width() * self.zoomX, self.currImageLabel.height() * self.zoomX, QtCore.Qt.KeepAspectRatioByExpanding)
         self.update()
+
+    def getImages(self):
+        ''' Get the names and paths of all the images in a directory. '''
+
+        VALID_FORMAT = ('.BMP', '.GIF', '.JPG', '.JPEG', '.PNG', '.PBM', '.PGM', '.PPM', '.TIFF', '.TIF', '.XBM')  # Image formats supported by Qt
+        id_pattern = "(p\d{1,4})" # Image id example: 'scan_Plate_R_{p03}_0_A02f00d4.TIF',
+
+        # Populate Bright Field image list
+        if os.path.isdir(self.folders["BF"]):
+            for file in os.listdir(self.folders["BF"]):
+                if file.upper().endswith(VALID_FORMAT):
+                    im_path = os.path.join(self.folders["BF"], file)
+
+                    match = re.search(id_pattern, file)
+                    id_ = match.group()            
+                    
+                    image_obj = {'name': file, 'path': im_path, 'type': "BF", 'id': id_}
+                    self.imageList["BF"].append(image_obj)
+        
+        # Populate Texas Red image list                    
+        if os.path.isdir(self.folders["TR"]):
+            for file in os.listdir(self.folders["TR"]):
+                if file.upper().endswith(VALID_FORMAT):
+                    im_path = os.path.join(self.folders["TR"], file)
+
+                    match = re.search(id_pattern, file)
+                    id_ = match.group()
+                    
+                    image_obj = {'name': file, 'path': im_path, 'type': "TF", 'id': id_}
+                    self.imageList["TR"].append(image_obj)      
+        return      
+
+    def selectDir(self):
+        ''' Select a directory, make list of images in it and display the first image in the list. '''
+        # open 'select folder' dialog box
+        self.folders["BASE"] = str(QtWidgets.QFileDialog.getExistingDirectory(self.window, "Select Directory")) + "/"
+        if not self.folders["BASE"]:
+            QtWidgets.QMessageBox.warning(self.window, 'No Folder Selected', 'Please select a valid Folder')
+            return
+
+        dirs = [dir_[0] for dir_ in os.walk(self.folders["BASE"])][1:]
+
+        dir_hr = [dir_.split("/")[-1] for dir_ in dirs] # Human-readable directories
+        dir_clean = list(map(str.strip, list(map(str.upper, dir_hr)))) # Stripped and trimmed dir_hr
+
+        for i in range(len(dir_clean)):
+            dir_ = dir_clean[i]
+            if "BF" == dir_:
+                self.folders["BF"] = dirs[i]
+            if "TEXAS RED" == dir_:
+                self.folders["TR"] = dirs[i]
+    
+        if self.folders["BF"] is None:            
+            QtWidgets.QMessageBox.warning(self.window, 'Missing Folder', 'Brightfield (BF) folder cannot be found. Please select directory with BF folder.')
+            return
+        elif self.folders["TR"] is None:
+            QtWidgets.QMessageBox.warning(self.window, 'Missing Folder', 'Texas Red folder cannot be found. Please select directory with Texas Red folder.')
+            return
+
+        self.getImages()
+        self.currImageList = self.imageList["BF"]
+        self.numImages = len(self.currImageList)
+
+        # make qitems of the image names
+        self.items = [QtWidgets.QListWidgetItem(log['name']) for log in self.currImageList]
+        self.window.qlist_images.clear()
+        for item in self.items:
+            self.window.qlist_images.addItem(item)
+
+        # Display first image and enable Pan 
+        self.currImageIdx = 0
+        self.enablePan(True)
+        self.loadImage(self.currImageList[self.currImageIdx]['path'])
+
+        self.items[self.currImageIdx].setSelected(True)
+
+        # Enable the next image button on the gui if multiple images are loaded
+        if self.numImages > 1:
+            self.window.next_im.setEnabled(True)
+
+    def resizeEvent(self, evt):
+        if self.currImageIdx >= 0:
+            self.onResize()
+
+    def nextImg(self):
+        if self.currImageIdx < self.numImages -1:
+            self.currImageIdx += 1
+            self.loadImage(self.currImageList[self.currImageIdx]['path'])
+            self.items[self.currImageIdx].setSelected(True)
+        else:
+            QtWidgets.QMessageBox.warning(self.window, 'Sorry', 'No more Images!')
+
+    def prevImg(self):
+        if self.currImageIdx > 0:
+            self.currImageIdx -= 1
+            self.loadImage(self.currImageList[self.currImageIdx]['path'])
+            self.items[self.currImageIdx].setSelected(True)
+        else:
+            QtWidgets.QMessageBox.warning(self.window, 'Sorry', 'No previous Image!')
+
+    def item_click(self, item):
+        self.currImageIdx = self.items.index(item)
+        self.loadImage(self.currImageList[self.currImageIdx]['path'])
+
+    def action_move(self):
+        if self.toggle_move.isChecked():
+            self.currImageLabel.setCursor(QtCore.Qt.OpenHandCursor)
+            self.enablePan(True)        
 
     def loadImage(self, imagePath):
         ''' To load and display new image.'''
         self.qimage = self.convertCvImage2QtImage(self.preprocessImg(imagePath))
-        self.qpixmap = QPixmap(self.qlabel_image.size())
+        self.qpixmap = QPixmap(self.currImageLabel.size())
         if not self.qimage.isNull():
             # reset Zoom factor and Pan position
             self.zoomX = 1
             self.position = [0, 0]
-            print(self.qlabel_image.width(), self.qlabel_image.height())
-            self.qimage_scaled = self.qimage.scaled(self.qlabel_image.width(), self.qlabel_image.height(), QtCore.Qt.KeepAspectRatioByExpanding)
+            
+            self.qimage_scaled = self.qimage.scaled(self.currImageLabel.width(), self.currImageLabel.height(), QtCore.Qt.KeepAspectRatioByExpanding)
             self.update()
         else:
-            self.statusbar.showMessage('Cannot open this image! Try another one.', 5000)
+            self.window.statusbar.showMessage('Cannot open this image! Try another one.', 5000)
 
     def update(self):
-        ''' This function actually draws the scaled image to the qlabel_image.
+        ''' This function actually draws the scaled image to the currImageLabel.
             It will be repeatedly called when zooming or panning.
             So, I tried to include only the necessary operations required just for these tasks. 
         '''
         if not self.qimage_scaled.isNull():
             # check if position is within limits to prevent unbounded panning.
             px, py = self.position
-            px = px if (px <= self.qimage_scaled.width() - self.qlabel_image.width()) else (self.qimage_scaled.width() - self.qlabel_image.width())
-            py = py if (py <= self.qimage_scaled.height() - self.qlabel_image.height()) else (self.qimage_scaled.height() - self.qlabel_image.height())
+            px = px if (px <= self.qimage_scaled.width() - self.currImageLabel.width()) else (self.qimage_scaled.width() - self.currImageLabel.width())
+            py = py if (py <= self.qimage_scaled.height() - self.currImageLabel.height()) else (self.qimage_scaled.height() - self.currImageLabel.height())
             px = px if (px >= 0) else 0
             py = py if (py >= 0) else 0
             self.position = (px, py)
@@ -72,10 +188,10 @@ class ImageViewer:
             painter = QPainter()
             painter.begin(self.qpixmap)
             painter.drawImage(QtCore.QPoint(0, 0), self.qimage_scaled,
-                    QtCore.QRect(self.position[0], self.position[1], self.qlabel_image.width(), self.qlabel_image.height()) )
+                    QtCore.QRect(self.position[0], self.position[1], self.currImageLabel.width(), self.currImageLabel.height()) )
             painter.end()
 
-            self.qlabel_image.setPixmap(self.qpixmap)
+            self.currImageLabel.setPixmap(self.qpixmap)
         else:
             pass
 
@@ -98,26 +214,26 @@ class ImageViewer:
     def zoomPlus(self):
         self.zoomX += 1
         px, py = self.position
-        px += self.qlabel_image.width()/2
-        py += self.qlabel_image.height()/2
+        px += self.currImageLabel.width()/2
+        py += self.currImageLabel.height()/2
         self.position = (px, py)
-        self.qimage_scaled = self.qimage.scaled(self.qlabel_image.width() * self.zoomX, self.qlabel_image.height() * self.zoomX, QtCore.Qt.KeepAspectRatioByExpanding)
+        self.qimage_scaled = self.qimage.scaled(self.currImageLabel.width() * self.zoomX, self.currImageLabel.height() * self.zoomX, QtCore.Qt.KeepAspectRatioByExpanding)
         self.update()
 
     def zoomMinus(self):
         if self.zoomX > 1:
             self.zoomX -= 1
             px, py = self.position
-            px -= self.qlabel_image.width()/2
-            py -= self.qlabel_image.height()/2
+            px -= self.currImageLabel.width()/2
+            py -= self.currImageLabel.height()/2
             self.position = (px, py)
-            self.qimage_scaled = self.qimage.scaled(self.qlabel_image.width() * self.zoomX, self.qlabel_image.height() * self.zoomX, QtCore.Qt.KeepAspectRatioByExpanding)
+            self.qimage_scaled = self.qimage.scaled(self.currImageLabel.width() * self.zoomX, self.currImageLabel.height() * self.zoomX, QtCore.Qt.KeepAspectRatioByExpanding)
             self.update()
 
     def resetZoom(self):
         self.zoomX = 1
         self.position = [0, 0]
-        self.qimage_scaled = self.qimage.scaled(self.qlabel_image.width() * self.zoomX, self.qlabel_image.height() * self.zoomX, QtCore.Qt.KeepAspectRatioByExpanding)
+        self.qimage_scaled = self.qimage.scaled(self.currImageLabel.width() * self.zoomX, self.currImageLabel.height() * self.zoomX, QtCore.Qt.KeepAspectRatioByExpanding)
         self.update()
 
     def enablePan(self, value):
--- imageAnalysis/PyQt-Image-Viewer/main.py
+++ imageAnalysis/PyQt-Image-Viewer/main.py
@@ -5,121 +5,33 @@
 from PyQt5 import QtCore, QtGui, QtWidgets, uic
 
 from ImageViewer import ImageViewer
-import sys, os
+import sys, os, re
 
 gui = uic.loadUiType("main.ui")[0]     # load UI file designed in Qt Designer
-VALID_FORMAT = ('.BMP', '.GIF', '.JPG', '.JPEG', '.PNG', '.PBM', '.PGM', '.PPM', '.TIFF', '.XBM', '.TIF')  # Image formats supported by Qt
-
-def getImages(folder):
-    ''' Get the names and paths of all the images in a directory. '''
-    image_list = []
-    if os.path.isdir(folder):
-        for file in os.listdir(folder):
-            if file.upper().endswith(VALID_FORMAT):
-                im_path = os.path.join(folder, file)
-                image_obj = {'name': file, 'path': im_path }
-                image_list.append(image_obj)
-    return image_list
 
 class Iwindow(QtWidgets.QMainWindow, gui):
     def __init__(self, parent=None):
         QtWidgets.QMainWindow.__init__(self, parent)
         self.setupUi(self)
 
-        self.cntr, self.numImages = -1, -1  # self.cntr have the info of which image is selected/displayed
-        
-        self.image_viewer = ImageViewer(self.qlabel_image)
+        imageLabels = (self.qlabel_img_bf, self.qlabel_img_tr)
+        self.imageViewer = ImageViewer(imageLabels, self)
         self.__connectEvents()
         self.showMaximized()
 
     def __connectEvents(self):
-        self.open_folder.clicked.connect(self.selectDir)
-        self.next_im.clicked.connect(self.nextImg)
-        self.prev_im.clicked.connect(self.prevImg)
-        self.qlist_images.itemClicked.connect(self.item_click)
+        self.open_folder.clicked.connect(self.imageViewer.selectDir)
+        self.next_im.clicked.connect(self.imageViewer.nextImg)
+        self.prev_im.clicked.connect(self.imageViewer.prevImg)
+        self.qlist_images.itemClicked.connect(self.imageViewer.item_click)
         # self.save_im.clicked.connect(self.saveImg)
 
-        self.zoom_plus.clicked.connect(self.image_viewer.zoomPlus)
-        self.zoom_minus.clicked.connect(self.image_viewer.zoomMinus)
-        self.reset_zoom.clicked.connect(self.image_viewer.resetZoom)
+        self.zoom_plus.clicked.connect(self.imageViewer.zoomPlus)
+        self.zoom_minus.clicked.connect(self.imageViewer.zoomMinus)
+        self.reset_zoom.clicked.connect(self.imageViewer.resetZoom)
 
-        self.toggle_move.toggled.connect(self.action_move)
+        self.toggle_move.toggled.connect(self.imageViewer.action_move)
 
-    def selectDir(self):
-        ''' Select a directory, make list of images in it and display the first image in the list. '''
-        # open 'select folder' dialog box
-        self.folder = str(QtWidgets.QFileDialog.getExistingDirectory(self, "Select Directory")) + "/"
-        if not self.folder:
-            QtWidgets.QMessageBox.warning(self, 'No Folder Selected', 'Please select a valid Folder')
-            return
-
-        dirs = [dir_[0] for dir_ in os.walk(self.folder)][1:]
-
-        dir_hr = [dir_.split("/")[-1] for dir_ in dirs] # Human-readable directories
-        dir_clean = list(map(str.strip, list(map(str.upper, dir_hr)))) # Stripped and trimmed dir_hr
-
-        for i in range(len(dir_clean)):
-            dir_ = dir_clean[i]
-            if "BF" == dir_:
-                self.image_viewer.folders["BF"] = dirs[i]
-            if "TEXAS RED" == dir_:
-                self.image_viewer.folders["TR"] = dirs[i]
-    
-        if self.image_viewer.folders["BF"] is None:            
-            QtWidgets.QMessageBox.warning(self, 'Missing Folder', 'Brightfield (BF) folder cannot be found. Please select directory with BF folder.')
-            return
-        elif self.image_viewer.folders["TR"] is None:
-            QtWidgets.QMessageBox.warning(self, 'Missing Folder', 'Texas Red folder cannot be found. Please select directory with Texas Red folder.')
-            return
-
-        self.logs = getImages(self.image_viewer.folders["BF"])
-        self.numImages = len(self.logs)
-
-        # make qitems of the image names
-        self.items = [QtWidgets.QListWidgetItem(log['name']) for log in self.logs]
-        for item in self.items:
-            self.qlist_images.addItem(item)
-
-        # Display first image and enable Pan 
-        self.cntr = 0
-        self.image_viewer.enablePan(True)
-        self.image_viewer.loadImage(self.logs[self.cntr]['path'])
-
-        self.items[self.cntr].setSelected(True)
-
-        # Enable the next image button on the gui if multiple images are loaded
-        if self.numImages > 1:
-            self.next_im.setEnabled(True)
-
-    def resizeEvent(self, evt):
-        if self.cntr >= 0:
-            self.image_viewer.onResize()
-
-    def nextImg(self):
-        if self.cntr < self.numImages -1:
-            self.cntr += 1
-            self.image_viewer.loadImage(self.logs[self.cntr]['path'])
-            self.qlist_images.setItemSelected(self.items[self.cntr], True)
-        else:
-            QtWidgets.QMessageBox.warning(self, 'Sorry', 'No more Images!')
-
-    def prevImg(self):
-        if self.cntr > 0:
-            self.cntr -= 1
-            self.image_viewer.loadImage(self.logs[self.cntr]['path'])
-            self.qlist_images.setItemSelected(self.items[self.cntr], True)
-        else:
-            QtWidgets.QMessageBox.warning(self, 'Sorry', 'No previous Image!')
-
-    def item_click(self, item):
-        # self.cntr = self.items.index(item)
-        pass
-        # self.image_viewer.loadImage(self.logs[self.cntr]['path'])
-
-    def action_move(self):
-        if self.toggle_move.isChecked():
-            self.qlabel_image.setCursor(QtCore.Qt.OpenHandCursor)
-            self.image_viewer.enablePan(True)
 
 def main():
     app = QtWidgets.QApplication(sys.argv)
